b33c4a87a3fb7f5368cc732f92d45c8a446d89c1
YANG <435355671@qq.com>
2018-02-13 09:08:53 +0800

    add .

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by ASUS on 2018/2/10.
+ */
+
+public class AQI {
+	public AQICity city;
+	public class AQICity{
+		public String aqi;
+		public String pm25;
+	}
+
+
+}
